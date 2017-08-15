b39528b64ab555108303bc4eaadc696074e73993
xingyufeifan <634846539@qq.com>
2017-08-15 18:51:46 +0800

    最终版本

--- app/src/main/java/com/nandi/disastermanager/MainActivity.java
+++ app/src/main/java/com/nandi/disastermanager/MainActivity.java
@@ -2023,6 +2023,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     personLocationGraphics.clear();
                 } else {
                     personLocation();
+                    layers.clear();
+                    elevationSources.clear();
+                    layers.add(new ArcGISMapImageLayer(getResources().getString(R.string.chongqing_url)));
                 }
                 break;
             case R.id.iv_search_main:
